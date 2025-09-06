a3fc2d7f9b9f217384893e10855c9c3cb24cc268
zprihoda <zhoda93@gmail.com>
2025-09-05 19:49:03 -0700

    Separate pytree types into separate module

---
+++ tests/test_pytrees.py
@@ -0,0 +1,271 @@
+import jax.numpy as jnp
+import pytest
+import zProj.pytrees as pytrees
+
+def test_Trajectory():
+    m = 2
+    n = 3
+    N = 4
+    xTraj = jnp.arange((N + 1) * n).reshape((N + 1, n))
+    uTraj = jnp.arange(N * m).reshape((N, m))
+    traj = pytrees.Trajectory(xTraj, uTraj)
+    assert jnp.all(traj.xTraj == xTraj)
+    assert jnp.all(traj.uTraj == uTraj)
+
+    for i in range(N):
+        assert jnp.all(traj[i].xTraj == xTraj[i])
+        assert jnp.all(traj[i].uTraj == uTraj[i])
+    assert jnp.all(traj[i + 1].xTraj == xTraj[N])
+
+
+def test_CostFunction():
+    runningCost = lambda x, u: x @ x + u @ u
+    terminalCost = lambda x: 2 * x @ x
+    CostFun = pytrees.CostFunction(runningCost, terminalCost)
+
+    traj = pytrees.Trajectory(jnp.array([[1, 2], [3, 4]]), jnp.array([[1, 1]]))
+    j0 = CostFun(traj, k=0)
+    J = CostFun(traj)
+    assert j0 == 7
+    assert J == 57
+
+
+def test_CostFunction_runningOnly():
+    runningCost = lambda x, u: x @ x + u @ u
+    CostFun = pytrees.CostFunction.runningOnly(runningCost, 2)
+
+    traj = pytrees.Trajectory(jnp.array([[1, 2], [3, 4]]), jnp.array([[1, 1]]))
+    j0 = CostFun(traj, k=0)
+    J = CostFun(traj)
+    assert j0 == 7
+    assert J == 32
+
+
+def test_QuadraticValueFunction():
+    v = jnp.array(1.)
+    v_x = jnp.array([2., 3])
+    v_xx = jnp.array([[4., 5], [6, 7]])
+    V = pytrees.QuadraticValueFunction(v, v_x, v_xx)
+    assert V.v == v
+    assert jnp.all(V.v_x == v_x)
+    assert jnp.all(V.v_xx == v_xx)
+
+    x = jnp.array([8., 9])
+    V_exp = 1 + 16 + 27 + 807.5
+    assert V(x) == pytest.approx(V_exp)
+
+
+def test_QuadraticValueFunction_fromTerminalCostFunction():
+
+    c = 1
+    c_x = jnp.array([1, 2])
+    c_xx = jnp.eye(2)
+    x0 = jnp.zeros(2)
+    costFun = pytrees.CostFunction(0, lambda x: c + c_x.T @ x + 0.5 * x.T @ c_xx @ x)
+    value = pytrees.QuadraticValueFunction.fromTerminalCostFunction(costFun, x0)
+    assert value.v == c
+    assert jnp.all(value.v_x == c_x)
+    assert jnp.all(value.v_xx == c_xx)
+
+
+def test_QuadraticCostFunction_base():
+    c = jnp.array(0.)
+    c_x = jnp.array([1, 2])
+    c_u = jnp.array([2, 1])
+    c_xx = jnp.eye(2)
+    c_xu = jnp.ones((2, 2))
+    c_uu = jnp.eye(2)
+    C = pytrees.QuadraticCostFunction(c, c_x, c_u, c_xx, c_xu, c_uu)
+    assert C.c == c
+    assert jnp.all(C.c_x == c_x)
+    assert jnp.all(C.c_u == c_u)
+    assert jnp.all(C.c_xx == c_xx)
+    assert jnp.all(C.c_xu == c_xu)
+    assert jnp.all(C.c_uu == c_uu)
+
+    x = jnp.array([1., 2])
+    u = jnp.array([3., 4])
+    C_exp = 0. + 5 + 10 + 2.5 + 21 + 12.5
+    assert C(x, u) == pytest.approx(C_exp)
+
+
+def test_QuadraticCostFunction_getItem():
+    c = jnp.array([1, 1])
+    c_x = jnp.eye(2)
+    c_u = jnp.eye(2)
+    c_xx = jnp.array([jnp.eye(2), jnp.zeros((2, 2))])
+    c_xu = jnp.array([jnp.eye(2), jnp.zeros((2, 2))])
+    c_uu = jnp.array([jnp.eye(2), jnp.zeros((2, 2))])
+    C = pytrees.QuadraticCostFunction(c, c_x, c_u, c_xx, c_xu, c_uu)
+
+    C0 = C[0]
+    assert C0.c == c[0]
+    assert jnp.all(C0.c_x == c_x[0])
+    assert jnp.all(C0.c_u == c_u[0])
+    assert jnp.all(C0.c_xx == c_xx[0])
+    assert jnp.all(C0.c_xu == c_xu[0])
+    assert jnp.all(C0.c_uu == c_uu[0])
+
+    C1 = C[1]
+    assert C1.c == c[1]
+    assert jnp.all(C1.c_x == c_x[1])
+    assert jnp.all(C1.c_u == c_u[1])
+    assert jnp.all(C1.c_xx == c_xx[1])
+    assert jnp.all(C1.c_xu == c_xu[1])
+    assert jnp.all(C1.c_uu == c_uu[1])
+
+
+def test_QuadraticCostFunction_fromFunction():
+    c = 1
+    c_x = jnp.array([1, 2])
+    c_u = jnp.array([2, 1])
+    c_xx = jnp.eye(2)
+    c_xu = jnp.array([[1, 2], [3, 4]])
+    c_uu = jnp.eye(2)
+    x0 = jnp.zeros(2)
+    u0 = jnp.zeros(2)
+
+    costFun = pytrees.CostFunction.runningOnly(
+        lambda x, u: c + c_x.T @ x + c_u.T @ u + 0.5 * (x.T @ c_xx @ x + 2 * x.T @ c_xu @ u + u.T @ c_uu @ u)
+    )
+    C = pytrees.QuadraticCostFunction.from_function(costFun, x0, u0)
+
+    assert C.c == c
+    assert jnp.all(C.c_x == c_x)
+    assert jnp.all(C.c_u == c_u)
+    assert jnp.all(C.c_xx == c_xx)
+    assert jnp.all(C.c_xu == c_xu)
+    assert jnp.all(C.c_uu == c_uu)
+
+
+def test_QuadraticCostFunction_fromTrajectory():
+    c = 1
+    c_x = jnp.array([1, 2])
+    c_u = jnp.array([2, 1])
+    c_xx = jnp.eye(2)
+    c_xu = jnp.array([[1, 2], [3, 4]])
+    c_uu = jnp.eye(2)
+    x0 = jnp.array([[0., 0], [1, 0]])
+    u0 = jnp.zeros((2, 2))
+    traj = pytrees.Trajectory(x0, u0)
+    costFun = pytrees.CostFunction.runningOnly(
+        lambda x, u: c + c_x.T @ x + c_u.T @ u + 0.5 * (x.T @ c_xx @ x + 2 * x.T @ c_xu @ u + u.T @ c_uu @ u)
+    )
+    C = pytrees.QuadraticCostFunction.from_trajectory(costFun, traj)
+
+    C0 = C[0]
+    assert C0.c == c
+    assert jnp.all(C0.c_x == c_x)
+    assert jnp.all(C0.c_u == c_u)
+    assert jnp.all(C0.c_xx == c_xx)
+    assert jnp.all(C0.c_xu == c_xu)
+    assert jnp.all(C0.c_uu == c_uu)
+
+    C1 = C[1]
+    x1 = x0[1]
+    assert C1.c == costFun(traj, k=1)
+    assert jnp.all(C1.c_x == c_x + c_xx @ x1)
+    assert jnp.all(C1.c_u == c_u + c_xu.T @ x1)
+    assert jnp.all(C1.c_xx == c_xx)
+    assert jnp.all(C1.c_xu == c_xu)
+    assert jnp.all(C1.c_uu == c_uu)
+
+
+def test_AffineDynamics_base():
+    f = jnp.array([1, 1])
+    f_x = jnp.array([[2, 3], [4, 5]])
+    f_u = jnp.array([[6], [7]])
+    dynamics = pytrees.AffineDynamics(f, f_x, f_u)
+    assert jnp.all(dynamics.f == f)
+    assert jnp.all(dynamics.f_x == f_x)
+    assert jnp.all(dynamics.f_u == f_u)
+
+    x = jnp.array([1, 2])
+    u = jnp.array([2])
+    xOut_exp = jnp.array([21, 29])
+    assert dynamics(x, u) == pytest.approx(xOut_exp)
+
+
+def test_AffineDynamics_getItem():
+    f = jnp.array([0, 1])
+    f_x = jnp.eye(2)
+    f_u = jnp.eye(2)
+    dyn = pytrees.AffineDynamics(f, f_x, f_u)
+
+    dyn0 = dyn[0]
+    assert dyn0.f == f[0]
+    assert jnp.all(dyn0.f_x == f_x[0])
+    assert jnp.all(dyn0.f_u == f_u[0])
+
+    dyn1 = dyn[1]
+    assert dyn1.f == f[1]
+    assert jnp.all(dyn1.f_x == f_x[1])
+    assert jnp.all(dyn1.f_u == f_u[1])
+
+
+def test_AffineDynamics_fromFunction():
+    f = jnp.array([1, 1])
+    f_x = jnp.array([[2, 3], [4, 5]])
+    f_u = jnp.array([[6], [7]])
+    dynFun = lambda x, u: f + f_x @ x + f_u @ u
+    x0 = jnp.zeros(2)
+    u0 = jnp.zeros(1)
+
+    dynamics = pytrees.AffineDynamics.from_function(dynFun, x0, u0)
+    assert jnp.all(dynamics.f == f)
+    assert jnp.all(dynamics.f_x == f_x)
+    assert jnp.all(dynamics.f_u == f_u)
+
+
+def test_AffineDynamics_fromTrajectory():
+    f = jnp.array([1, 1])
+    f_x = jnp.array([[2, 3], [4, 5]])
+    f_u = jnp.array([[6], [7]])
+    dynFun = lambda x, u: f + f_x @ x + f_u @ u + 0.5 * x.T @ x
+    x0 = jnp.array([[0., 0], [1, 0]])
+    u0 = jnp.zeros((2, 1))
+    traj = pytrees.Trajectory(x0, u0)
+
+    dynamics = pytrees.AffineDynamics.from_trajectory(dynFun, traj)
+    assert jnp.all(dynamics[0].f == f)
+    assert jnp.all(dynamics[0].f_x == f_x)
+    assert jnp.all(dynamics[0].f_u == f_u)
+    assert jnp.all(dynamics[1].f == dynFun(x0[1], u0[1]))
+    assert jnp.all(dynamics[1].f_x == f_x + x0[1])
+    assert jnp.all(dynamics[1].f_u == f_u)
+
+
+def test_AffinePolicy_base():
+    l = jnp.array([1, 2])
+    L = jnp.array([[1, 2], [3, 4]])
+    policy = pytrees.AffinePolicy(l, L)
+
+    x = jnp.array([1, 2])
+    u_exp = jnp.array([6, 13])
+    assert jnp.all(policy(x) == u_exp)
+
+
+def test_AffinePolicy_multi():
+    n = 2
+    m = 3
+    N = 2
+    l = jnp.arange(0, N * m).reshape((N, m))
+    L = jnp.arange(0, N * m * n).reshape((N, m, n))
+    x = jnp.array([0, 0])
+    policy = pytrees.AffinePolicy(l, L)
+
+    assert jnp.all(policy[1].l == l[1])
+    assert jnp.all(policy[0].L == L[0])
+    assert jnp.all(policy(x, k=0) == l[0])
+    assert jnp.all(policy(x, k=1) == l[1])
+
+
+def test_QuadraticDeltaCost():
+    dJ_lin = 1
+    dJ_quad = 2
+    dJFun = pytrees.QuadraticDeltaCost(dJ_lin, dJ_quad)
+    assert dJFun(1) == 3
+    assert dJFun(0.5) == 1
+
+
+
---
+++ zProj/pytrees.py
@@ -0,0 +1,146 @@
+import jax
+import jax.numpy as jnp
+from typing import Callable, NamedTuple
+
+
+class Trajectory(NamedTuple):
+    """Trajectory tuple: (xTraj, uTraj)"""
+    xTraj: jnp.ndarray
+    uTraj: jnp.ndarray
+
+    def __getitem__(self, k: int):
+        return jax.tree.map(lambda x: x[k], self)
+
+
+class CostFunction(NamedTuple):
+    """
+    Cost function tuple: (runningCost, terminalCost)
+    ```
+    J = terminalCost(x[N]) + sum(runningCost(x[i],u[i]))
+    ```
+    """
+    runningCost: Callable[[jnp.ndarray, jnp.ndarray], float]
+    terminalCost: Callable[[jnp.ndarray], float]
+
+    @classmethod
+    def runningOnly(cls, runningCost: Callable[[jnp.ndarray, jnp.ndarray], float], m: int = 1):
+        terminalCost = lambda x: runningCost(x, jnp.zeros(m))
+        return cls(runningCost, terminalCost)
+
+    def __call__(self, traj: Trajectory, k=None):
+        runningCost, terminalCost = self
+        xTraj, uTraj = traj
+        return jax.vmap(runningCost)(xTraj[:-1],
+                                     uTraj) + terminalCost(xTraj[-1]) if k is None else runningCost(xTraj[k], uTraj[k])
+
+
+class QuadraticValueFunction(NamedTuple):
+    """Value function of the form: `V(x) = v + v_x.T @ x + 0.5 * (x.T @ v_xx @ x)`"""
+    v: jnp.ndarray
+    v_x: jnp.ndarray
+    v_xx: jnp.ndarray
+
+    def __call__(self, x: jnp.ndarray):
+        v, v_x, v_xx = self
+        return v + v_x.T @ x + 0.5 * x.T @ v_xx @ x
+
+    @classmethod
+    def fromTerminalCostFunction(cls, costFun: CostFunction, x0: jnp.array):
+        cf = costFun.terminalCost
+        v = cf(x0)
+        v_x = jax.grad(cf)(x0)
+        v_xx = jax.hessian(cf)(x0)
+        return cls(v, v_x, v_xx)
+
+
+class QuadraticCostFunction(NamedTuple):
+    """
+    Quadratic cost function of the form:
+    ```
+    C(x,u) = c + c_x.T @ x + c_u.T @ u + 0.5 * (x.T @ c_xx @ x + 2*x.T @ c_xu @ u + u.T @ c_uu @ u)
+    ```
+    """
+    c: jnp.ndarray
+    c_x: jnp.ndarray
+    c_u: jnp.ndarray
+    c_xx: jnp.ndarray
+    c_xu: jnp.ndarray
+    c_uu: jnp.ndarray
+
+    @classmethod
+    def from_function(cls, costFun: CostFunction, x0: jnp.ndarray, u0: jnp.ndarray):
+        """Second order Taylor series expansion of cost function `c(x,u)` about (x0,u0)"""
+        runningCost = costFun.runningCost
+        c = runningCost(x0, u0)
+        c_x, c_u = jax.jacobian(runningCost, argnums=(0, 1))(x0, u0)
+        ((c_xx, c_xu), (_, c_uu)) = jax.hessian(runningCost, (0, 1))(x0, u0)
+        return cls(c, c_x, c_u, c_xx, c_xu, c_uu)
+
+    @classmethod
+    def from_trajectory(cls, costFun: CostFunction, traj: Trajectory):
+        """Second order Taylor series expansion of cost function `c(x,u)` about (xTraj,uTraj)"""
+        xTraj, uTraj = traj
+        return jax.vmap(lambda x0, u0: cls.from_function(costFun, x0, u0))(xTraj, uTraj)
+
+    def __call__(self, x: jnp.ndarray, u: jnp.ndarray, k: int = None):
+        c, c_x, c_u, c_xx, c_xu, c_uu = self
+        if k is None and c.ndim != 0:
+            raise ValueError("Must specify index for multi-dimensional cost")
+        return c + c_x @ x + c_u @ u + 0.5 * (x.T @ c_xx @ x + 2 * x.T @ c_xu @ u +
+                                              u.T @ c_uu @ u) if k is None else self[k](x, u)
+
+    def __getitem__(self, k: int):
+        return jax.tree.map(lambda x: x[k], self)
+
+
+class AffineDynamics(NamedTuple):
+    """Affine dynamics of the form: `xOut = f + f_x @ x + f_u @ u`"""
+    f: jnp.ndarray
+    f_x: jnp.ndarray
+    f_u: jnp.ndarray
+
+    @classmethod
+    def from_function(cls, dynFun: Callable[[jnp.ndarray, jnp.ndarray], jnp.ndarray], x0: jnp.ndarray, u0: jnp.ndarray):
+        """First order Taylor series expansion of the dynamics function `f(x,u)` about (x0,u0)"""
+        f = dynFun(x0, u0)
+        f_x = jax.jacobian(dynFun, 0)(x0, u0)
+        f_u = jax.jacobian(dynFun, 1)(x0, u0)
+        return cls(f, f_x, f_u)
+
+    @classmethod
+    def from_trajectory(cls, dynFun: Callable[[jnp.ndarray, jnp.ndarray], jnp.ndarray], traj: Trajectory):
+        """Second order Taylor series expansion of cost function `c(x,u)` about (xTraj,uTraj)"""
+        xTraj, uTraj = traj
+        return jax.vmap(lambda x0, u0: cls.from_function(dynFun, x0, u0))(xTraj, uTraj)
+
+    def __call__(self, x: jnp.ndarray, u: jnp.ndarray, k: int = None):
+        f, f_x, f_u = self
+        if k is None and f.ndim != 1:
+            raise ValueError("Must specify index for multi-dimensional dynamics")
+        return f + f_x @ x + f_u @ u if k is None else self[k](x, u)
+
+    def __getitem__(self, k: int):
+        return jax.tree.map(lambda x: x[k], self)
+
+
+class AffinePolicy(NamedTuple):
+    l: jnp.ndarray
+    L: jnp.ndarray
+
+    def __call__(self, x: jnp.ndarray, k: int = None, alpha: float = 1):
+        l, L = self
+        if k is None and l.ndim != 1:
+            raise ValueError("Must specify index for multi-dimensional policy")
+        return alpha * l + L @ x if k is None else self[k](x)
+
+    def __getitem__(self, k: int):
+        return jax.tree.map(lambda x: x[k], self)
+
+
+class QuadraticDeltaCost(NamedTuple):
+    dJ_lin: float
+    dJ_quad: float
+
+    def __call__(self, alpha):
+        dJ_lin, dJ_quad = self
+        return alpha * (dJ_lin + alpha * dJ_quad)
