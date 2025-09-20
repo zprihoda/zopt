866de7608a32ca693b7ad7bd8e8bb7266ea23d41
zprihoda <zhoda93@gmail.com>
2025-09-19 19:52:10 -0700

    Add tests for plotMpcTrajectory

--- tests/test_mpcUtils.py
+++ tests/test_mpcUtils.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pytest
+
 import zopt.mpcUtils as mpc
 
 
@@ -19,3 +21,25 @@ def test_lqrMpc():
     prob = mpc.lqrMpc(A, B, Q, R, N, x_lb, x_ub, u_lb, u_ub)
     u, traj, status = prob.solve(x0)
     assert status == "optimal"
+
+
+def test_plotMpcTrajectory():
+    N_t = 2
+    N_mpc = 3
+    n = 4
+    dt = 0.1
+    traj = np.zeros((N_t, N_mpc, n))
+
+    fig, ax = mpc.plotMpcTrajectory(traj, dt)
+    plt.close(fig)
+
+
+def test_plotMpcTrajectory2():
+    N_t = 2
+    N_mpc = 3
+    n = 4
+    dt = 0.1
+    traj = np.zeros((N_t, N_mpc, n))
+
+    fig, ax = mpc.plotMpcTrajectory(traj, dt, names=['a', 'b', 'c', 'd'], title="foobar")
+    plt.close(fig)
