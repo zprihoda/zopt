d345cfe1adbf3e63adafadf56e839f3d03f9c5ae
zprihoda <zhoda93@gmail.com>
2025-09-26 20:41:29 -0700

    Initial pass at inequality and equality constraints

--- tests/test_scpUtils.py
+++ tests/test_scpUtils.py
@@ -5,7 +5,7 @@ from zopt.pytrees import Trajectory
 from zopt.scpUtils import sequentialConvexProgramming
 
 @pytest.mark.filterwarnings("ignore::PendingDeprecationWarning")
-def test_sequentialConvexProgramming():
+def test_sequentialConvexProgramming_base():
     A = np.eye(2)
     B = np.eye(2)
     Q = np.eye(2)
@@ -22,4 +22,53 @@ def test_sequentialConvexProgramming():
     terminalCost = lambda x: x @ Q @ x
 
     traj, converged = sequentialConvexProgramming(traj0, x0, f, runningCost, terminalCost, dt)
+    assert converged
+
+@pytest.mark.filterwarnings("ignore::PendingDeprecationWarning")
+def test_sequentialConvexProgramming_ineq():
+    A = np.eye(2)
+    B = np.eye(2)
+    Q = np.eye(2)
+    R = np.eye(2)
+    N = 3
+    dt = 0.1
+    x0 = np.ones(2)
+    xTraj0 = np.ones((N + 1, 2))
+    uTraj0 = np.zeros((N, 2))
+    traj0 = Trajectory(xTraj0, uTraj0)
+
+    f = lambda x, u: A @ x + B @ u
+    runningCost = lambda x, u: x @ Q @ x + u @ R @ u
+    terminalCost = lambda x: x @ Q @ x
+    f_ineq = [
+        lambda x, u: -x[0],     # x >= 0
+        lambda x, u: -x[1],
+        lambda x, u: x[0] - 1.1, # x <= 1.1
+        lambda x, u: x[1] - 1.1
+        ]
+
+    traj, converged = sequentialConvexProgramming(traj0, x0, f, runningCost, terminalCost, dt, ineqConstraints=f_ineq)
+    assert converged
+
+@pytest.mark.filterwarnings("ignore::PendingDeprecationWarning")
+def test_sequentialConvexProgramming_eq():
+    A = np.eye(2)
+    B = np.eye(2)
+    Q = np.eye(2)
+    R = np.eye(2)
+    N = 3
+    dt = 0.1
+    x0 = np.ones(2)
+    xTraj0 = np.ones((N + 1, 2))
+    uTraj0 = np.zeros((N, 2))
+    traj0 = Trajectory(xTraj0, uTraj0)
+
+    f = lambda x, u: A @ x + B @ u
+    runningCost = lambda x, u: x @ Q @ x + u @ R @ u
+    terminalCost = lambda x: x @ Q @ x
+    f_eq = [
+        lambda x, u: x[0] - 1,     # x[0] == 0
+        ]
+
+    traj, converged = sequentialConvexProgramming(traj0, x0, f, runningCost, terminalCost, dt, eqConstraints=f_eq)
     assert converged
--- zopt/scpUtils.py
+++ zopt/scpUtils.py
@@ -1,12 +1,16 @@
 """
 TODO:
-- Control/state bound constraints?
-- Replace terminal cost with terminal state?
-- Add trust region constraints
+- Cleanup general inequality and equality constraints
+    - Create a LOCP pytree object with "from_trajectory" function for linearization
+    - Also add a discretization function and cleanup dynamics in cvx problem
+        - x + dt*(f + f_x @ x + f_u @ u) = dt*f + (dt*f_x + I) @ x + dt*f_u @ u
+    - Consider making constraints time/frame dependent (which would also support terminal constraints)
+- Add trust region constraints, eg. `npl.norm(xTrajPrev - xPrev) <= rho_x`
 - See Stanford HW4:obstacle_avoidance for other options to include
 """
 
 import cvxpy as cvx
+import jax
 import numpy as np
 
 from typing import Callable
@@ -14,39 +18,62 @@ from typing import Callable
 from zopt.pytrees import Trajectory, AffineDynamics
 
 
-def _setupProblem(N, n, m, dt, runningCost, terminalCost, x0):
+def _setupProblem(N, n, m, dt, runningCost, terminalCost, x0, n_ineq, n_eq):
     """
     Setup the cvx problem with tuneable parameters
 
-    NOTE: The parameters are flattened from 3D to 2D arrays to be compatible with cvxpys canonicalization backend which
-        doesn't support > 2D variables / parameters. This will be fixed in a future release, in which case we can
-        un-flatten f_x and f_u.
+    NOTE: The parameters are flattened from 3D to 2D arrays to be compatible with cvxpy's canonicalization backend which
+        doesn't support > 2D variables / parameters. This will be fixed in a future release of cvxpy, in which case we
+        can un-flatten the parameters.
     """
     x = cvx.Variable((N + 1, n), name="x")
     u = cvx.Variable((N, m), name="u")
     g = lambda x, u: dt * runningCost(x, u)
     gf = terminalCost
+
     f0 = cvx.Parameter((N, n), name="f0")
     f_x = cvx.Parameter((N, n*n), name="f_x")
     f_u = cvx.Parameter((N, n*m), name="f_u")
 
     cost = gf(x[-1]) + cvx.sum([g(x[k], u[k]) for k in range(N)])
     objective = cvx.Minimize(cost)
-
     dyn = lambda x, u, k: x[k] + dt * (f0[k] + f_x[k].reshape((n,n), 'C') @ x[k] + f_u[k].reshape((n,m), 'C') @ u[k])
-
     constraints = [x[0] == x0]
     constraints += [x[k + 1] == dyn(x, u, k) for k in range(N)]
+
+    # Add additional constraints
+    if n_ineq > 0:
+        f0_ineq = cvx.Parameter((N, n_ineq), name="f0_ineq")
+        f_x_ineq = cvx.Parameter((N, n_ineq*n), name="f_x_ineq")
+        f_u_ineq = cvx.Parameter((N, n_ineq*m), name="f_u_ineq")
+        constraints += [f0_ineq[k] + f_x_ineq[k].reshape((n_ineq,n), 'C') @ x[k] + f_u_ineq[k].reshape((n_ineq,m), 'C') @ u[k] <= 0 for k in range(N)]
+
+    if n_eq > 0:
+        f0_eq = cvx.Parameter((N, n_eq), name="f0_eq")
+        f_x_eq = cvx.Parameter((N, n_eq*n), name="f_x_eq")
+        f_u_eq = cvx.Parameter((N, n_eq*m), name="f_u_eq")
+        constraints += [f0_eq[k] + f_x_eq[k].reshape((n_eq,n), 'C') @ x[k] + f_u_eq[k].reshape((n_eq,m), 'C') @ u[k] == 0 for k in range(N)]
+
     prob = cvx.Problem(objective, constraints)
     return prob
 
 
-def _solveProblem(prob, affine_dynamics):
+def _solveProblem(prob, affine_dynamics, affine_ineq, affine_eq):
     # Updates prob parameters and solves
     N,n,m = affine_dynamics.f_u.shape
     prob.param_dict['f0'].value = np.asarray(affine_dynamics.f0)
-    prob.param_dict['f_x'].value = np.asarray(affine_dynamics.f_x).reshape((N,n*n))
-    prob.param_dict['f_u'].value = np.asarray(affine_dynamics.f_u).reshape((N,n*m))
+    prob.param_dict['f_x'].value = np.asarray(affine_dynamics.f_x).reshape((N,-1))
+    prob.param_dict['f_u'].value = np.asarray(affine_dynamics.f_u).reshape((N,-1))
+
+    if len(affine_ineq) > 0:
+        prob.param_dict['f0_ineq'].value = np.asarray(affine_ineq.f0)
+        prob.param_dict['f_x_ineq'].value = np.asarray(affine_ineq.f_x).reshape((N, -1))
+        prob.param_dict['f_u_ineq'].value = np.asarray(affine_ineq.f_u).reshape((N, -1))
+
+    if len(affine_eq) > 0:
+        prob.param_dict['f0_eq'].value = np.asarray(affine_eq.f0)
+        prob.param_dict['f_x_eq'].value = np.asarray(affine_eq.f_x).reshape((N, -1))
+        prob.param_dict['f_u_eq'].value = np.asarray(affine_eq.f_u).reshape((N, -1))
 
     J = prob.solve()
 
@@ -63,6 +90,8 @@ def sequentialConvexProgramming(
     runningCost: Callable[[np.ndarray, np.ndarray], float],
     terminalCost: Callable[[np.ndarray], float],
     dt: float,
+    ineqConstraints: list[Callable[[np.ndarray, np.ndarray], float]] = [],
+    eqConstraints: list[Callable[[np.ndarray, np.ndarray], float]] = [],
     tol: float = 1e-3,
     maxIter=100
 ) -> tuple[Trajectory, bool]:
@@ -79,7 +108,10 @@ def sequentialConvexProgramming(
     runningCost : Running cost function of the form `j = runningCost(x,u)`
     terminalCost : Terminal cost function of the form `jf = terminalCost(xf)`
     dt : Discretization time step
+    ineqConstraints : List of inequality constraints of the form: `f_i(x,u) <= 0`
+    eqConstraints : List of equality constraints of the form: `h_i(x,u) = 0`
     tol : Convergence tolerance
+    maxIter : maximum number of SCP iterations to run
 
     Returns
     -------
@@ -90,13 +122,33 @@ def sequentialConvexProgramming(
 
     n = traj.xTraj.shape[1]
     N, m = traj.uTraj.shape
+    n_ineq = len(ineqConstraints)
+    n_eq = len(eqConstraints)
 
     converged = False
-    cvxProb = _setupProblem(N, n, m, dt, runningCost, terminalCost, x0)
+    cvxProb = _setupProblem(N, n, m, dt, runningCost, terminalCost, x0, n_ineq, n_eq)
     J_prev = np.inf
     for i in range(maxIter):
         affine_dynamics = AffineDynamics.from_trajectory(dynamics, traj)
-        traj, J = _solveProblem(cvxProb, affine_dynamics)
+
+        affine_ineq = jax.tree.map(lambda f_i: AffineDynamics.from_trajectory(f_i, traj), ineqConstraints)
+        if n_ineq > 0:
+            affine_ineq = AffineDynamics(
+                np.stack([x.f0 for x in affine_ineq], axis=1),
+                np.stack([x.f_x for x in affine_ineq], axis=1),
+                np.stack([x.f_u for x in affine_ineq], axis=1)
+            )
+
+        affine_eq = jax.tree.map(lambda f_i: AffineDynamics.from_trajectory(f_i, traj), eqConstraints)
+        if n_eq > 0:
+            affine_eq = AffineDynamics(
+                np.stack([x.f0 for x in affine_eq], axis=1),
+                np.stack([x.f_x for x in affine_eq], axis=1),
+                np.stack([x.f_u for x in affine_eq], axis=1)
+            )
+
+        traj, J = _solveProblem(cvxProb, affine_dynamics, affine_ineq, affine_eq)
+
         if abs(J - J_prev) < tol:
             converged = True
             break
